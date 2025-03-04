e80ab61ee4b9faba998ca6faac16f888a9d3d6a0
Steven Roussey <sroussey@gmail.com>
2025-03-04 09:41:44 -0800

    feat: Add dependency injection (DI) support to utility package
    - Implement Container class for managing service instances and dependencies
    - Create ServiceRegistry for type-safe service registration and retrieval
    - Export DI modules across browser, node, and bun entry points
    - Provide global container and service registry instances

--- packages/util/src/di/index.ts
+++ packages/util/src/di/index.ts
@@ -5,14 +5,5 @@
 //    *   Licensed under the Apache License, Version 2.0 (the "License");           *
 //    *******************************************************************************
 
-export * from "./graph";
-export * from "./utilities/Misc";
-export * from "./events/EventEmitter";
-export * from "./crypto/Crypto.node";
-export * from "./utilities/TypeUtilities";
-export * from "./di";
-import bettersqlite from "better-sqlite3";
-
-export const Sqlite: { Database: typeof bettersqlite } = {
-  Database: bettersqlite,
-};
+export * from "./Container";
+export * from "./ServiceRegistry";
