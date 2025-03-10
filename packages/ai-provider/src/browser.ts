b105871f0b41ba27ec7d754c5bd206b520206cf5
Steven Roussey <sroussey@gmail.com>
2025-03-10 13:52:55 -0700

    refactor: Remove platform-specific AI provider export files

--- packages/ai-provider/src/browser.ts
+++
@@ -1,9 +0,0 @@
-//    *******************************************************************************
-//    *   ELLMERS: Embedding Large Language Model Experiential Retrieval Service    *
-//    *                                                                             *
-//    *   Copyright Steven Roussey <sroussey@gmail.com>                             *
-//    *   Licensed under the Apache License, Version 2.0 (the "License");           *
-//    *******************************************************************************
-
-// Export browser-compatible providers
-export * from "./common";
